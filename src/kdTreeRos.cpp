fc761035db57d510c2fd9207b990e611acfcccc2
telemc97 <telemc97@gmail.com>
2023-11-11 00:25:13 +0200

    daily update

--- src/kdTreeRos.cpp
+++ src/kdTreeRos.cpp
@@ -8,16 +8,18 @@ kdTreeRos::kdTreeRos(const ros::NodeHandle &nh_):
   node_handle(nh_) 
 {
   node_handle.param("resolution", res, res);
-
   tree = new kdtree_ns::KdTree();
-
   sub = new message_filters::Subscriber<kd_tree_ros::DetectionStamped>(node_handle, "/tracker_output", 5);
-  
 }
 
-bool kdTreeRos::insertData(DetectionStamped det){
+bool kdTreeRos::insertData(kd_tree_ros::DetectionStamped det_msg){
+  coord_ns::Coord coord;
+  coord.sec = det_msg.header.stamp.sec;
+  coord.nsec = det_msg.header.stamp.nsec;
+  coord.x = det_msg.point.x;
+  coord.y = det_msg.point.y;
+  coord.z = det_msg.point.z;
+  tree->insertPoint(coord, det_msg.confidence, det_msg.id, det_msg.class_name);
 
 }
-
-
 }
